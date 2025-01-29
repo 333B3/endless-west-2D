1f0970211e52004dca452e355700f7a33b04033b
Kindinov Ivan <135726796+333B3@users.noreply.github.com>
2025-01-29 23:33:12 +0200

    ,

--- Game/Script/bandit.gd
+++ Game/Script/bandit.gd
@@ -1,66 +1,44 @@
 extends CharacterBody2D
 
 var speed = 50
-var direction = Vector2.ZERO
-var change_direction = 1.0
-var timer = 1.0
-var stop_timer = 2
-var is_stopped = false
-
-var heath = 100 
-var damage = 10
-var dead = false
 var player_in_area = false
 var player
-
+var dead = false
 
 func _ready():
 	randomize()
-	var dead = false
+	dead = false
 
-func _physics_process(_delta):
-	timer -= _delta
-	if timer <= 0:
-		if is_stopped:
-			#direction = random_direction()
-			timer = change_direction
-			is_stopped = false
-		else:
-			direction = Vector2.ZERO
-			timer = stop_timer
-			is_stopped = true
-
-	velocity = direction * speed
-	if !dead:
-		$detector/CollisionShape2D.disabled = false
-		if player_in_area:
-			position += (player.position - position) / speed
+func _physics_process(delta):
 	if dead:
 		$detector/CollisionShape2D.disabled = true
+		return
+
+	# Если игрок в зоне обнаружения, двигаемся к нему
+	if player_in_area and player:
+		var direction = (player.position - position).normalized()
+		velocity = direction * speed
+	else:
+		# Если игрок не в зоне, останавливаемся
+		velocity = Vector2.ZERO
+
 	move_and_slide()
-	#play_animation()
+	play_animation()
 
+func play_animation():
+	if velocity == Vector2.ZERO:
+		$AnimatedSprite2D.play("idle")
+	else:
+		$AnimatedSprite2D.play("run")
 
-#func random_direction() -> Vector2:
-	#var x = randi() % 3 - 1
-	#var y = randi() % 5 - 1
-	#var new_direction = Vector2(x, y).normalized()
-	#if new_direction != Vector2.ZERO:
-		#return new_direction
-	#else:
-		#return random_direction()
-#
-#func play_animation():
-	#if is_stopped:
-		#$AnimatedSprite2D.play("idle")
-	#else:
-		#$AnimatedSprite2D.play("run")
-
-func _on_detector_body_entered(body):
-	if body.has_method("player"):
+func _on_detector_area_entered(area):
+	print("Player entered area!")  # Отладочное сообщение
+	if area.is_in_group("player"):
 		player_in_area = true
-		player = body
+		player = area.get_parent()
+		print("Player object:", player)  # Отладочное сообщение
 
-func _on_detector_body_exited(body):
-	if body.has_method("player"):
+func _on_detector_area_exited(area):
+	print("Player left area!")  # Отладочное сообщение
+	if area.is_in_group("player"):
 		player_in_area = false
