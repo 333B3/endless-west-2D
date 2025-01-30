782c39858c977bd68309d746b0884db3a649732e
Kindinov Ivan <135726796+333B3@users.noreply.github.com>
2025-01-30 21:22:56 +0200

    .

--- Game/Script/bandit.gd
+++ Game/Script/bandit.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 var speed = 50
-var change_direction = 1.0
-var timer = 1.0
+var change_direction = 2
+var timer = 1.5
 var stop_timer = 2
 var is_stopped = false
 
@@ -12,30 +12,34 @@ var dead = false
 var player_in_area = false
 
 var player: Node2D
+var min_distance = 15
 
 func _ready():
 	randomize()
 	dead = false
+	player_in_area = false
 
 func _physics_process(delta):
-	if player_in_area:
-		print("GO GO GO")
-		var direction = player_direction()  
-		velocity = speed * direction  
+	if player_in_area and player:
+		var direction = player_direction()
+		var distance = global_position.distance_to(player.global_position)
+		
+		if distance > min_distance: 
+			velocity = speed * direction
+		else:  
+			velocity = Vector2.ZERO
 	else:
 		timer -= delta
 		if timer <= 0:
 			if is_stopped:
-				var direction = random_direction()  
+				var direction = random_direction()
 				velocity = direction * speed
 				timer = change_direction
 				is_stopped = false
 			else:
-				var direction = Vector2.ZERO  
-				velocity = direction * speed
+				velocity = Vector2.ZERO
 				timer = stop_timer
 				is_stopped = true
-
 
 	move_and_slide()
 	play_animation()
@@ -50,23 +54,22 @@ func random_direction() -> Vector2:
 		return random_direction()
 
 func play_animation():
-	if is_stopped:
+	if velocity == Vector2.ZERO:
 		$AnimatedSprite2D.play("idle")
 	else:
 		$AnimatedSprite2D.play("run")
 
 func player_direction() -> Vector2:
-	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player:
 		return (player.global_position - global_position).normalized()
 	return Vector2.ZERO
 
-
 func _on_detector_area_entered(area):
-	print("Player entered area!")  
-	player_in_area = true
-
+	if area.is_in_group("player"):
+		player_in_area = true
+		player = area.get_parent()
 
 func _on_detector_area_exited(area):
-	print("Player left area!")  
-	player_in_area = false
+	if area.is_in_group("player"):
+		player_in_area = false
+		player = null
