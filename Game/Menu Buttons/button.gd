4c797d1ffa9a95b1dcfc4045f6afc3e98cf6b631
Kindinov Ivan <135726796+333B3@users.noreply.github.com>
2025-04-03 21:52:44 +0300

    .

--- Game/Menu Buttons/button.gd
+++ Game/Menu Buttons/button.gd
@@ -6,4 +6,5 @@ extends Button
 func _on_pressed():
 	#animation_player.play("FadeOut")  # Анімація затемнення
 	#await animation_player.animation_finished  # Чекаємо завершення
+	
 	get_tree().change_scene_to_file("res://Game/Main_Map/main_loc.tscn")
