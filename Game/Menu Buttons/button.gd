35bd275beda21718f1f0067e93435a1cbebd9be7
Kindinov Ivan <135726796+333B3@users.noreply.github.com>
2025-04-03 20:59:24 +0300

    new

--- Game/Menu Buttons/button.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-# Назва сцени, яку потрібно відкрити
-const GAME_SCENE = "res://Game/World/World.tscn"
-
-func _on_pressed():
-	get_tree().change_scene_to_file(GAME_SCENE)
--- Game/Menu Buttons/button_4.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-# Функція викликається при натисканні кнопки
-func _on_pressed():
-	get_tree().quit()
