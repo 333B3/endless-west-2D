c36415beba5e180ed0002db588c015176ae1b383
Kindinov Ivan <135726796+333B3@users.noreply.github.com>
2025-05-15 00:40:07 +0300

    .

--- Game/Scene/ui_menu_in_game.gd
+++ Game/Scene/ui_menu_in_game.gd
@@ -21,15 +21,9 @@ func CheckESC():
 func _on_resume_pressed():
 	resume()
 
-#func _on_main_menu_pressed():
-	#if get_tree().paused:
-		#get_tree().paused = false
-	#SceneManager.change_scene(
-		#"res://Game/Menu Buttons/menu.tscn", {
-			#"pattern_enter" : "circle",
-			#"pattern_leave" : "fade",
-		#}
-	#)
+func _on_main_menu_pressed():
+	var mainMenu = load("res://Game/Menu_Buttons/menu.tscn")
+	get_tree().change_scene_to_packed(mainMenu)
 
 func _on_setting_pressed():
 	pass # Replace with function body.
