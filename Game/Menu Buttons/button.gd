672f0a2dc7440d67e395f25f9ad519e8b5720e23
ANTON ODYNOCHENKO <115722015+Barni135@users.noreply.github.com>
2025-05-20 13:56:45 +0300

    1

---
+++ Game/Menu Buttons/button.gd
@@ -0,0 +1,13 @@
+extends Button
+
+@onready var animation_player = $"../AnimationPlayer"
+
+
+#func _on_pressed():
+	#SceneManager.change_scene(
+		#"res://Game/Main_Map/main_loc.tscn", {
+			#"pattern_enter" : "circle",
+			#"pattern_leave" : "fade",
+			#
+		#}
+	#)
