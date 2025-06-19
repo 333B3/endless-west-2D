142b60c4e8b2ad373502312757e57e7a38182701
Kindinov Ivan <135726796+333B3@users.noreply.github.com>
2025-06-19 23:05:40 +0300

    .

--- Game/Script/menu.gd
+++ Game/Script/menu.gd
@@ -3,7 +3,16 @@ extends Control
 func _ready():
 	var mainLoc = load("res://Game/Main_Map/main_loc.tscn")
 	var Setting_menu = load("res://Game/Scene/Setting_menu.tscn")
+	music_fx_start()
+	
+func _process(delta):
+	pass
 
+func music_fx_start():
+	$background_music.play()
+	
+func music_fx_stop():
+	$background_music.stop()
 func _on_new_pressed():
 	var mainLoc = load("res://Game/Main_Map/main_loc.tscn")
 	get_tree().change_scene_to_packed(mainLoc)
