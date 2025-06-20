9c89edc488509393137f394c67bc8bf6896db98d
ANTON ODYNOCHENKO <115722015+Barni135@users.noreply.github.com>
2025-06-20 19:41:53 +0300

    1

---
+++ Game/build/build.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name BuildingData
+
+@export var name: String
+@export var scene: PackedScene
+@export var preview_scene: PackedScene
+@export var required_resources: Dictionary 
