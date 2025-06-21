2ad5528809cae88c813a8cd63763f30e42f939f8
ANTON ODYNOCHENKO <115722015+Barni135@users.noreply.github.com>
2025-06-22 00:43:33 +0300

    1

--- Game/Script/classes/Inventory.gd
+++ Game/Script/classes/Inventory.gd
@@ -6,37 +6,36 @@ signal item_added(item)
 @export var Item: Dictionary = {}
 @export var slotcount: int = 20
 
-# Додаємо предмет до інвентарю
 func add_item(item: Item):
 	for i in Item.keys():
 		if Item[i]["item"] == item:  
 			Item[i]["count"] += 1
 			emit_signal("item_added", item)
+			print("Після додавання (існуючий): ", Item)
 			return
 
 	for i in range(slotcount):
 		if not Item.has(i):
 			Item[i] = {"item": item, "count": 1}
 			emit_signal("item_added", item)
+			print("Після додавання (новий): ", Item)
 			return
 
-# Видаляємо предмет з інвентарю
 func remove_item(item: Item):
 	for key in Item.keys():
 		if Item[key]["item"] == item:
 			Item[key]["count"] -= 1
 			if Item[key]["count"] <= 0:
 				Item.erase(key)
+			print("Після видалення: ", Item)
 			break
 
-# Перевіряємо, чи є предмет в інвентарі
 func has_item(item: Item) -> bool:
 	for key in Item.keys():
 		if Item[key]["item"] == item:
 			return true
 	return false
 
-# Повертаємо список усіх унікальних предметів
 func get_items():
 	var unique_items = []
 	for key in Item.keys():
@@ -44,10 +43,27 @@ func get_items():
 			unique_items.append(Item[key]["item"])
 	return unique_items
 
-# Екіпіруємо предмет у збройний слот
 func equip_item(item, weapon_slot_node):
 	if has_item(item):
 		weapon_slot_node.item = item
-		print("Екіпіровано зброю: ", item.ItemName)
+		print("Екіпіровано зброю: ", item.item_name)
 	else:
 		print("Предмет не в інвентарі, екіпірування скасовано")
+
+func remove_item_by_name(name: String, count_to_remove: int) -> void:
+	for key in Item.keys():
+		var entry = Item[key]
+		if entry["item"].item_name == name:
+			var count = entry["count"]
+			if count >= count_to_remove:
+				entry["count"] -= count_to_remove
+				var item_ref = entry["item"]
+				if entry["count"] <= 0:
+					Item.erase(key)
+				else:
+					Item[key] = entry
+				emit_signal("item_added", item_ref)  # Оновлення UI
+				print("Видалено ", count_to_remove, " одиниць ", name)
+				return
+			else:
+				print("Недостатньо кількості для видалення: ", count, " < ", count_to_remove)
--- Game/Script/classes/inventory_button.gd
+++ Game/Script/classes/inventory_button.gd
@@ -4,24 +4,40 @@ extends CanvasLayer
 @onready var toggle_button := $ToggleButton
 @onready var weapon_slot := $WeaponSlot/slot  
 @onready var place_button = $PlaceModeButton
+@onready var inv_node: Inventory = $Node  # Використовуємо @onready для безпечної ініціалізації
+
 var slot: Array = []
 var inventory_open := false
-var inv_node: Inventory = null
+
+signal build_button_pressed
+signal inventory_updated(inventory)
 
 func _ready():
 	inventory_panel.visible = false
 	toggle_button.pressed.connect(_on_toggle_button_pressed)
 
-	# Додаємо всі слоти
 	for i in $hotbar.get_children():
 		slot.append(i)
 	for i in $Inventory.get_children():
 		slot.append(i)
 
-	inv_node = get_node("Node")
 	if inv_node:
-		inv_node.item_added.connect(_on_item_added)
+		if not inv_node.is_connected("item_added", Callable(self, "_on_item_added")):
+			inv_node.item_added.connect(_on_item_added)
 		update_inventory_slots()
+		emit_signal("inventory_updated", inv_node)
+		print("UI: Інвентар ініціалізований: ", inv_node, " | ID: ", inv_node.get_instance_id(), " | Item: ", inv_node.Item)
+	else:
+		push_error("❌ UI: inv_node не знайдено!")
+
+func set_inventory(inventory: Inventory):
+	inv_node = inventory
+	if inv_node:
+		if not inv_node.is_connected("item_added", Callable(self, "_on_item_added")):
+			inv_node.item_added.connect(_on_item_added)
+		update_inventory_slots()
+		emit_signal("inventory_updated", inv_node)
+		print("UI: Інвентар встановлено ззовні: ", inv_node, " | ID: ", inv_node.get_instance_id(), " | Item: ", inv_node.Item)
 
 func _on_toggle_button_pressed() -> void:
 	_toggle_inventory()
@@ -32,11 +48,14 @@ func _toggle_inventory():
 
 func _on_item_added(item: Item):
 	update_inventory_slots()
+	emit_signal("inventory_updated", inv_node)
+	print("UI: Предмет додано: ", item.item_name, " | Об’єкт: ", item.get_instance_id(), " | Інвентар: ", inv_node.get_instance_id(), " | Item: ", inv_node.Item)
 
 func update_inventory_slots():
 	for s in slot:
 		s.item = null
 		s.count = 0
+		s.modulate = Color.WHITE
 
 	if inv_node:
 		var i = 0
@@ -45,13 +64,16 @@ func update_inventory_slots():
 			if entry.has("item") and i < slot.size():
 				slot[i].item = entry["item"]
 				slot[i].count = entry["count"]
+				slot[i].modulate = Color.GREEN if entry["item"].item_name == "Wood" else Color.WHITE
+				print("Слот UI ", i, ": item_name = ", entry["item"].item_name, " | count = ", entry["count"], " | Колір: ", slot[i].modulate)
 				i += 1
 
-# Перевірка чи є зброя в слоті
 func get_weapon_in_slot():
 	return weapon_slot.item if weapon_slot.item != null and weapon_slot.item.is_in_group("weapon") else null
 
-signal build_button_pressed
 func _on_place_mode_button_pressed() -> void:
 	print("Натиснута кнопка побудови")
 	emit_signal("build_button_pressed")
+
+func get_inventory() -> Inventory:
+	return inv_node
